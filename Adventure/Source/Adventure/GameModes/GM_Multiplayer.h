59474c72f8847a5da84de88cc6f45dfe70516c64
Eric Marquez <kenkm963@gmail.com>
2019-01-18 01:29:29 -0800

    net update

--- Adventure/Source/Adventure/GameModes/GM_Multiplayer.h
+++ Adventure/Source/Adventure/GameModes/GM_Multiplayer.h
@@ -46,6 +46,9 @@ protected:
 	// Callback for when seamless travel is finished
 	virtual void PostSeamlessTravel() override;
 
+	// Callback for when seamless travel is finished
+	virtual void PostLogin(APlayerController * NewPlayer);
+
 private:
 
 	// Generates a playerID to be able to refrence separate instances
