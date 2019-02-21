06b1de71717974f9b61dbdc095aa9a496c3dfb97
Knnth3 <kenkm963@gmail.com>
2019-02-20 21:12:19 -0800

    Trying out new download system

---
+++ Adventure/Source/Adventure/DownloadManager/DownloadManager.cpp
@@ -0,0 +1,66 @@
+// By: Eric Marquez. All information and code provided is free to use and can be used comercially.Use of such examples indicates no fault to the author for any damages caused by them. The author must be credited.
+
+#include "DownloadManager.h"
+#include "Adventure.h"
+
+#define PACKET_SIZE 1024
+
+ADownloadManager::ADownloadManager()
+{
+	bReplicates = true;
+	m_dataSize = 0;
+}
+
+void ADownloadManager::SetData(const TArray<uint8>& data)
+{
+	// Empty old data
+	m_data.Empty();
+	m_dataSize = data.Num();
+
+	int unpackedCount = data.Num();
+	int packetCount = FMath::DivideAndRoundUp(data.Num(), PACKET_SIZE);
+	for (int index = 0; index < packetCount; index++)
+	{
+		// Calculate chunk data
+		int size = (unpackedCount < PACKET_SIZE) ? unpackedCount : PACKET_SIZE;
+		unpackedCount -= PACKET_SIZE;
+
+		// Create new chunk
+		FDownloadChunk newChunk;
+		newChunk.PacketID = index;
+		newChunk.Data.Insert(&data[index*PACKET_SIZE], size, 0);
+
+		// Add chunk to download buffer
+		m_data.Push(newChunk);
+	}
+}
+
+TArray<uint8> ADownloadManager::GetUnpackedData() const
+{
+	TArray<uint8> Unpacked;
+	for (const auto& chunk : m_data)
+	{
+		Unpacked.Insert(chunk.Data, Unpacked.Num());
+	}
+
+	return Unpacked;
+}
+
+void ADownloadManager::OnDataReceived()
+{
+	UE_LOG(LogNotice, Warning, TEXT("<DownloadManager>: New Data received. Data size: %i"), m_data.Num());
+
+	if (m_dataSize == m_data.Num())
+	{
+		UE_LOG(LogNotice, Warning, TEXT("<DownloadManager>: Download completed"));
+	}
+}
+
+void ADownloadManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ADownloadManager, m_data);
+	DOREPLIFETIME(ADownloadManager, m_dataSize);
+}
+
